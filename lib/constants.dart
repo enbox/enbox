9b8d99e23202335869c9d49b0064c5c75a866e0b
Sreehari S <sreeharisreedev1@gmail.com>
2020-04-11 15:41:34 -0700

    added constants and restructured code

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class EnboxConstants extends InheritedWidget {
+  static EnboxConstants of(BuildContext context) => context.dependOnInheritedWidgetOfExactType<EnboxConstants>();
+  const EnboxConstants({Widget child, Key key}): super(key: key, child: child);
+
+  final String displayName = "enbox";
+  final Color color = Colors.blue;
+  final String demoName = "Enbox Demo";
+  final String demoEmail = "enbox@enbox.com";
+  @override
+  bool updateShouldNotify(EnboxConstants oldWidget) => false;
+}
